abb7fb244935d5f8885761ad9c7248b1413f44aa
Bosnot <setefanoboston@gmail.com>
2024-05-15 17:34:21 +1200

    Added double roll for first roll of round
    At the start of each round, two dice are rolled. If both rolls are equal, there is an opportunity for double points.

--- C_02_Random_integer.py
+++ C_02_Random_integer.py
@@ -1,5 +1,31 @@
 import random
 
+def roll_die():
+  result = random.randint(1, 6)
+  return result
+
+die_roll = roll_die()
+print(die_roll)
+
 for i in range(1, 10):
-  roll = random.randint(1, 3)
-  print(roll, end="\t")
+    double_point = 0
+    double_roll = 0
+
+    roll_1 = roll_die()
+    roll_2 = roll_die()
+
+    if roll_1 == roll_2:
+      double_point += 1
+
+    double_roll = roll_1 + roll_2
+
+    print(f"Dice 1: {roll_1}, Dice 2: {roll_2}, Total sum: {double_roll}")
+
+    if double_point == 1:
+      print('You have rolled doubles! Double points opportunity!')
+
+
+
+
+
+
